ed86cd0272afec4e95cef5e73f13f1cbb2554647
Lucas Aho <aholucas11@gmail.com>
2019-11-30 14:17:15 -0700

    Basic layout and contact page set up, firebase is linked to contacts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,50 @@
-var portItems = [
-    wordGuess = {
-        name: "HangMan Game",
-        url: "https://lucasaho.github.io/Word-Guess-Game/",
-        lesson: "javascript"
-    }, 
-    crystalCollect = {
-        name: "Crystal Collector",
-        url: "https://lucasaho.github.io/unit-4-game/",
-        lesson: "jQuery"
-    }, 
-    triviaGame = {
-        name: "Animal Trivia Game",
-        url: "https://lucasaho.github.io/TriviaGame/",
-        lesson: "javascript timekeeping"
-    }, 
-    giphyAPI = {
-        name: "Giphy API",
-        url: "https://lucasaho.github.io/Giphy-Week-Six/",
-        lesson: "API implementation"
-    }, 
-    trainSchedule = {
-        name: "Train Scheduler",
-        url: "https://lucasaho.github.io/Train-Scheduler/",
-        lesson: "Firebase and data storage"
-    },
-    project1 = {
-        name: "Colorado Ski and Board Report",
-        url: "https://lucasaho.github.io/Project1/",
-        lesson: "Team management and combining skills"
+//Contact info is stored in Firebase until better method learned
+var firebaseConfig = {
+    apiKey: "AIzaSyB06pMMLLO-FUx3wUwJVfmsE9VEpLlCBB0",
+    authDomain: "portfolio-contact-page-5016a.firebaseapp.com",
+    databaseURL: "https://portfolio-contact-page-5016a.firebaseio.com",
+    projectId: "portfolio-contact-page-5016a",
+    storageBucket: "portfolio-contact-page-5016a.appspot.com",
+    messagingSenderId: "854579126431",
+    appId: "1:854579126431:web:fb4b200a9a677c2ff602aa"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+ 
+//Function for email validation
+const validate = (email) => {
+    const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    
+    if (expression.test(String(email).toLowerCase()) === false) {
+        $("#email-input").attr("placeholder", "Please use a valid email");
+    } else {
+        $("#email-input").attr("placeholder", "example@gmail.com");
+        var emailUser = {
+            userEmail: email,
+            userMsg: $("#msg-input").val().trim(),
+            userName: $("#name-input").val().trim(),
+        }
+
+        $("#name-input").val("");
+        $("#msg-input").val("");
+        database.ref().push(emailUser);
     }
-]
-portItems.forEach(function(element){
-    var halfCol = "<div class='col-5 m-2 pb-5 border border-white'>";
-    var portDiv = "<div id='portDiv'>";
-    var portHead = "<h2 class='text-center'>" + element.name + "</h2>"
-    var portP = "<p>Project built proficiency in " + element.lesson + "</p>"
-    $("#portBlock").append(halfCol + portDiv + portHead + portP + "</div></div>")
+}
+
+//On click for message submit button
+$("#msg-submit").on("click", function(){
+    event.preventDefault();
+    var emailRaw = $("#email-input").val().trim()
+    console.log(emailRaw);
+    $("#email-input").val("");
+    validate(emailRaw);
+});
+
+//On click for Contact --> Home Button
+$("home-btn").on("click", function () {
+    event.preventDefault();
+    
 });
